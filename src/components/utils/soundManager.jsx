f55580adc2fcf31d1d624dc84bf20e573c5232d7
Kritika Chahar <kcundercover4eva@gmail.com>
2025-12-26 23:23:36 +0000

    File changes

--- src/components/utils/soundManager.jsx
+++ src/components/utils/soundManager.jsx
@@ -47,7 +47,7 @@ export const soundManager = new SoundManager();
 
 // Ambient sound URLs (loopable ambient tracks)
 const ambientSounds = {
-  rain: 'https://archive.org/download/DeepForestWindAndRain1Hour/Deep%20Forest%20Wind%20And%20Rain%20%281%20Hour%29.mp3',
+  rain: 'https://ia600400.us.archive.org/21/items/DeepForestWindAndRain1Hour/Deep%20Forest%20Wind%20And%20Rain%20%281%20Hour%29.mp3',
   cafe: 'https://archive.org/download/1-hour-relaxing-jazz-coffee-shop-music-the-best-melodies-that-will-warm-your-heart/1%20Hour%20Relaxing%20Jazz%20Coffee%20Shop%20Music%20%20The%20Best%20Melodies%20That%20Will%20Warm%20Your%20Heart.mp3',
   whitenoise: 'https://assets.mixkit.co/active_storage/sfx/2395/2395.wav',
   synth: 'https://archive.org/download/youtube-g6hY7dB54bc/2%20Hour%20Synthwave%20MIX%20-%20L.A.%20Sunset%20%20Royalty%20Free%20Copyright%20Safe%20Music-g6hY7dB54bc.mp3',
